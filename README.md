660b27a16e77410bafe3852615f65e3354476615
零风PLFJY <gxpjy2008@163.com>
2025-06-21 23:46:09 +0800

    Auto Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# neo-bpsys-docs
+
+Documations of [neo-bpsys-wpf](https://github.com/PLFJY/neo-bpsys-wpf/)
+
--- publish.bat
+++ publish.bat
@@ -1,5 +1,5 @@
-docfx docfx.json
 git pull
+docfx docfx.json
 git add .
 git commit -m "Auto Commit"
 git push
